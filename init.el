48854d1ae6c316a76062f168fd4dc3c755e31ec8
Thiago Polastri <thiagopolastri@gmail.com>
2023-04-07 01:50:27 -0300

    Update comment

--- init.el
+++ init.el
@@ -19,10 +19,10 @@
     gcs-done)))
 
 ;; elpaca will get stuck on multi-vterm building task, but everything works.
-;; package first run  - 155.02 seconds with 5 garbage collections
-;; package normal run - 2.64 seconds with 2 garbage collections
-;; elpaca first run   - 13.69 seconds with 2 garbage collections
-;; elpaca normal run  - 0.32 seconds with 7 garbage collections
+;; package first run  - 184.74 seconds with 5 garbage collections
+;; package normal run - 2.92 seconds with 2 garbage collections
+;; elpaca first run   - 12.84 seconds with 1 garbage collections
+;; elpaca normal run  - 0.26 seconds with 1 garbage collections
 
 ;; (require 'dotemacs-package) ; Replace :elpaca nil with :ensure nil
 (require 'dotemacs-elpaca) ; Replace :ensure nil with :elpaca nil
@@ -39,6 +39,7 @@
 ;; hacky and private stuff that I dont want to commit.
 (load (dotemacs-get-path "user.el") :no-error) ; 0.07s
 
+;; TODO: maybe do this on early-init with after-init-hook
 (run-with-idle-timer 4 nil
                      (lambda ()
                        (setq gc-cons-threshold  67108864) ; 64M
